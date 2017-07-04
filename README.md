8081508f9485970d6e69621cdc59ddbf87f8a265
Neonix <Neonix@lovewow.fr>
2017-07-04 02:31:52 -0700

    update game init

---
+++ README.md
@@ -0,0 +1,10 @@
+todpole
+=================
+
+1. php start.php start -d
+
+2. http://ip:8383 
+
+
+
+
---
+++ start.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * run with command 
+ * php start.php start
+ */
+
+ini_set('display_errors', 'on');
+use Workerman\Worker;
+
+if(strpos(strtolower(PHP_OS), 'win') === 0)
+{
+    exit("start.php not support windows, please use start_for_win.bat\n");
+}
+
+if(!extension_loaded('pcntl'))
+{
+    exit("Please install pcntl extension. See http://doc3.workerman.net/install/install.html\n");
+}
+
+if(!extension_loaded('posix'))
+{
+    exit("Please install posix extension. See http://doc3.workerman.net/install/install.html\n");
+}
+
+
+define('GLOBAL_START', 1);
+
+require_once __DIR__ . '/Workerman/Autoloader.php';
+
+foreach(glob(__DIR__.'/Applications/*/start*.php') as $start_file)
+{
+    require_once $start_file;
+}
+
+Worker::runAll();
